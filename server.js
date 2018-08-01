40fb9a1acb558d7fe54eab85cca4f968b9a8a9a7
fiona <fionamiaojing@gmail.com>
2018-08-01 12:20:27 -0700

    add AboutMe as another endpoint for static file serving

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-
+app.use('/AboutMe/', express.static(path.join(__dirname, 'public')));
 
 const port = process.env.port || 3005;
 app.listen(port, console.log(`App is Listenting to ${port}`));
