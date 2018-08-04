d159cb06af458b8133a8db62194ba7d031460987
fiona <fionamiaojing@gmail.com>
2018-08-04 11:34:15 -0700

    add some description and css stylr

--- client/projects.jsx
+++ client/projects.jsx
@@ -10,6 +10,9 @@ const Project = () => {
                 -- A Diary App for writing stories, mark ideas, choose feelings for dialy use.
             </div>
             <img src="/AboutMe/public/diaryDemo.gif" alt="diary app demo" width="375" height="812" />
+            <div className='tech'>
+                Technologies: React Native/Redux, Node.js/Express, Firebase, Google Cloud App Engine, MongoDB
+            </div>
             <br/>
             <hr/>
             <br/>
@@ -17,9 +20,12 @@ const Project = () => {
                 Itsy
             </div>
             <div className='pdes'>
-                -- A Clone of Etsy
+                -- Full-stack online B2C purchasing app 
             </div>
             <img src="/AboutMe/public/itsyDemo.gif" alt="Itsy app demo" width="640" height="360"/>
+            <div className='tech'>
+                Technologies: React/Redux, Node.js/Express, AWS, ECS, Docker, Jest, Mocha/Chai, MongoDB, Mongoose
+            </div>
             <br/>
             <hr/>
             <br/>
@@ -27,14 +33,18 @@ const Project = () => {
                 AbleTable
             </div>
             <div className='pdes'>
-                -- A Clone of OpenTable
+                -- Back-end of a restaurant reservation app
             </div>
             <div>
                 Throughput and Latency Demo
             </div>
             <br/>
             <img src="/AboutMe/public/Abletable.png" alt="backend performance demo" width="640" height="360"/>
+            <div className='tech'>
+                Technologies: AWS/EC2, Docker, Nginx, Redis, Loader.io, Artillery, New Relic, CircleCI, Mocha/Chai, Node.js/Express, PostgreSQL
+            </div>
             <hr/>
+            <br/>
             <div className='ptitle'>
                 Twittler
             </div>
@@ -42,7 +52,11 @@ const Project = () => {
                 -- A chat app
             </div>
             <img src="/AboutMe/public/twittler.gif" alt="Twittler demo"/>
+            <div className='tech'>
+                Technologies: HTML, CSS, Javascript, JQuery
+            </div>
             <hr/>
+            <br/>
             <div className='ptitle'>
                 IsTodayFriday
             </div>
@@ -51,6 +65,9 @@ const Project = () => {
             </div>
             <img src="/AboutMe/public/isTodayFriday.gif" alt=""/>
             <br/>
+            <div className='tech'>
+                Technologies: Swift
+            </div>
             <div className='end'>
                 To be continued....
             </div>
